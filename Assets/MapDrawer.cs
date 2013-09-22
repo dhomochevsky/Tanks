8bfb4723deaec54ccd96e0a0e8f10c72ea5d2f23
Mitchell Rivera <rivera.mitchell@gmail.com>
2013-09-22 03:18:36 -0700

    add map parsing and rendering

--- Assets/MapDrawer.cs
+++ Assets/MapDrawer.cs
@@ -1,20 +1,83 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using System.Collections.Generic;
+using JsonFx.Json;
+using JsonFx.Serialization;
+using UnityEngine;
+using System.Runtime.Serialization;
+using JsonFx.Serialization.Resolvers;
 
 public class MapDrawer : MonoBehaviour
 {
     public GameObject tile;
 
+    [DataContract]
+    private class MapFile
+    {
+        [DataMember] public Dictionary<string, GameObjectDefinition> tiles;
+
+        [DataMember] public int mapSize;
+
+        [DataMember] public string[] map;
+    }
+
+    [DataContract]
+    private class GameObjectDefinition
+    {
+        // if tanks or bullets stop when hitting it
+        [DataMember] public bool collidable = false;
+
+        // how far your tank slides after you let go of controls
+        [DataMember] public int slipperyness = 0;
+
+        // if you bullet does less than this much piercing, doesn't deal damage
+        [DataMember] public int armor = 0;
+
+        [DataMember] public string sprite = "";
+    }
+
+
+    public GameObject[,] map;
+
 	// Use this for initialization
 	void Start ()
 	{
-	    var newTile = Instantiate(tile, new Vector3(0, 0), Quaternion.identity) as GameObject;
-
-	    newTile.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("brick");
+        loadMapFile("map");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void loadMapFile(string fileName)
+    {
+	    var map = Resources.Load<TextAsset>(fileName);
+
+        var reader = new JsonReader(new DataReaderSettings(new DataContractResolverStrategy()));
+        var mapFile = reader.Read<MapFile>(map.text);
+        if (mapFile.map.Length != Math.Pow(mapFile.mapSize, 2))
+        {
+            throw new Exception("invalid map size");
+        }
+
+        this.map = new GameObject[mapFile.mapSize,mapFile.mapSize];
+        for (var row = 0; row < mapFile.mapSize; row++)
+        {
+            for (var column = 0; column < mapFile.mapSize; column++)
+            {
+                var tileName = mapFile.map[row*mapFile.mapSize + column];
+                this.map[row, column] = makeGameObject(mapFile.tiles[tileName].sprite, row, column);
+            }
+        }
+    }
+
+    GameObject makeGameObject(string texture, int x, int y)
+    {
+        Debug.Log(texture + " " + x + " " + y);
+	    var newGameObject = Instantiate(tile, new Vector3((float)x / 2, (float)y / 2), Quaternion.identity) as GameObject;
+
+	    newGameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(texture);
+
+        return newGameObject;
+    }
 }
--- Assets/Tile.cs
+++ Assets/Tile.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Runtime.Serialization;
 
-public class Tile : MonoBehaviour {
+public class Tile : MonoBehaviour
+{
+    // if tanks or bullets stop when hitting it
+    public bool collidable = false;
+
+    // how far your tank slides after you let go of controls
+    public int slipperyness = 0;
+
+    // if you bullet does less than this much piercing, doesn't deal damage
+    public int armor = 0;
+
+    public string sprite = "";
 
 	// Use this for initialization
 	void Start () {
