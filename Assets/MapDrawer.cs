facdd748d76e88ac3a3f53c77e0303b8e5044860
Mitchell Rivera <rivera.mitchell@gmail.com>
2013-09-22 00:11:51 -0700

    draw a brick

---
+++ Assets/MapDrawer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDrawer : MonoBehaviour
+{
+    public GameObject tile;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var newTile = Instantiate(tile, new Vector3(0, 0), Quaternion.identity) as GameObject;
+
+	    newTile.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("brick");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
